f624f5ed1bed94e7ffdaf4cb19ba0d9cdf39a4bd
Manmoh4n <90148527+Manmoh4n@users.noreply.github.com>
2023-02-13 13:44:20 +0530

    Add files via upload

---
+++ course_details.py
@@ -0,0 +1,10 @@
+# import os , sys                   # It is use when Error occur 
+# from os.path import dirname , join , abspath
+# sys.path.insert(0, abspath(join(dirname(__file__),'..')))
+
+from payment import payment_details
+
+def course():
+    print("This is my course details ")
+
+payment_details.payment()
